6d0fa5b712a043373e7f4e3f8ce7dfdefc3c9589
Artemov-glebb <128042031+Artemov-glebb@users.noreply.github.com>
2023-04-13 22:00:37 +0300

    hjkhjk
    jhkhj

---
+++ Assets/Scenes/что это такое и зачем оно мне надо.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class чтоэтотакоеизачемономненадо : MonoBehaviour
+{
+    public GameObject Testobj;
+    public void Deactive()
+    {
+        Testobj.SetActive(false);
+    }
+
+}
